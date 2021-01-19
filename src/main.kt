3605494c8d900bb410fbab05f46df94f55e35622
ArtemKorol1999 <artem.korolevskay@gmail.com>
2021-01-19 19:43:31 +0700

    TEST 1

---
+++ src/main.kt
@@ -0,0 +1,4 @@
+fun main()
+{ var smail = ("# # # # # # # #\n# # # # # # # #\n# O # # # # O #\n# # # # # # # #\n# O # # # # O #\n# # O O O O # #\n# # # # # # # #\n# # # # # # # #")
+print (smail)
+}
