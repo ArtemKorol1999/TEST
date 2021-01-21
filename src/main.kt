18fd1ce7b6da2a1173a43af4e7f4db67efb20fad
ArtemKorol1999 <artem.korolevskay@gmail.com>
2021-01-21 18:11:26 +0700

    TEST 2

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,6 @@
 fun main()
-
 { var smail = ("# # # # # # # #\n# # # # # # # #\n# O # # # # O #\n# # # # # # # #\n# O # # # # O #\n# # O O O O # #\n# # # # # # # #\n# # # # # # # #")
-print (smail)
+    
 
 
 
